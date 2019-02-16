c66990fcba5c26b2b5557c4f5cdbbdccb04d428f
Vova Femiak <vfemyak@gmail.com>
2019-02-16 17:18:46 +0200

    added loggers; fixed issues

---
+++ src/main/java/BO/SendMessageBO.java
@@ -0,0 +1,57 @@
+package BO;
+
+import POM.GmailHomePage;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SendMessageBO {
+
+    private GmailHomePage gmailHomePage;
+    private static Logger logger = LogManager.getLogger(SendMessageBO.class);
+
+    public SendMessageBO (WebDriver driver){
+        gmailHomePage = new GmailHomePage(driver);
+    }
+
+    public void sendMessage (WebDriver driver, String to, String subject, String message){
+        writeLetter(driver,to,subject,message);
+        gmailHomePage.sendMessage();
+        logger.info("Message has been sent");
+    }
+
+    public void writeMessageAndClose (WebDriver driver, String to, String subject, String message){
+        writeLetter(driver,to,subject,message);
+        gmailHomePage.saveMessage();
+        (new WebDriverWait(driver,10))      //waiting for closing letter form
+                .until(ExpectedConditions.invisibilityOfElementLocated
+                        (By.xpath("//td[@class=\'Hm\']/img[@class=\'Ha\']")));
+        logger.info("Message has been closed");
+    }
+
+    public void writeLetter (WebDriver driver, String to, String subject, String message){
+        gmailHomePage.clickCompose();
+        (new WebDriverWait(driver,10))      //waiting for opening letter form
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//td[@class=\'Hm\']/img[@class=\'Ha\']")));
+        gmailHomePage.writeLetter(to,subject,message);
+        logger.info("Message has been written");
+    }
+
+    public void checkAndSendDraftMessage(WebDriver driver, String to, String subject, String message){
+        gmailHomePage.openDraftFolder();
+        logger.info("Draft folder has been opened");
+        gmailHomePage.openDraftMessage();
+        (new WebDriverWait(driver,10))      //waiting for opening letter form
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//table[@class=\'IZ\']/descendant::*[@role=\'button\']")));
+        gmailHomePage.checkMessageFields(to, subject, message);
+        logger.info("Message has been checked");
+        gmailHomePage.sendMessage();
+        logger.info("Message has been sent");
+    }
+}
---
+++ src/main/java/POM/GmailHomePage.java
@@ -0,0 +1,79 @@
+package POM;
+
+import elements.Button;
+import elements.TextArea;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import wrappers.CustomFieldDecorator;
+
+public class GmailHomePage {
+
+    @FindBy(xpath = "//div[@class=\'z0\']/descendant::div[@role=\'button\']")
+    private Button composeButton;
+
+    @FindBy(css = "div.wO textarea.vO")
+    private TextArea toTextarea;
+
+    @FindBy(xpath = "//div[@class='oL aDm az9']/span[@email]")
+    private TextArea toTextareaCompare;
+
+    @FindBy(xpath = "//input[@name=\'subjectbox\']")
+    private TextArea subjectTextarea;
+
+    @FindBy(xpath = "//table[@class=\'iN\']/descendant::*[@role=\'textbox\']")
+    private TextArea messageTextarea;
+
+    @FindBy(xpath = "//td[@class=\'Hm\']/img[@class=\'Ha\']")
+    private Button saveAndCloseButton;
+
+    @FindBy(css = "a[href=\'https://mail.google.com/mail/u/0/#drafts\']")
+    private Button draftFolder;
+
+    @FindBy(xpath = "//div[@gh=\'tl\']//tr[1]")     //select first draft
+    private Button lastDraftMessage;
+
+    @FindBy(xpath = "//table[@class=\'IZ\']/descendant::*[@role=\'button\']")
+    private Button sendButton;
+
+    public GmailHomePage(WebDriver driver){
+        PageFactory.initElements(new CustomFieldDecorator(driver),this);
+    }
+
+    public void clickCompose(){
+        composeButton.click();
+    }
+
+    public void writeLetter(String to, String subject, String message){
+        toTextarea.sendKeys(20, to);
+        subjectTextarea.sendKeys(subject);
+        messageTextarea.sendKeys(message);
+    }
+
+    public void saveMessage(){
+        saveAndCloseButton.click();
+    }
+
+    public void openDraftFolder(){
+        draftFolder.click();
+    }
+
+    public void openDraftMessage(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        lastDraftMessage.click();
+    }
+    public void sendMessage(){
+        sendButton.click();
+    }
+
+    public void checkMessageFields(String to, String subject, String message){
+        //TODO: перемістити асерти в тест
+//        Assert.assertEquals(to,toTextareaCompare.getEmail());
+//        Assert.assertEquals(subject,subjectTextarea.getValue());
+//        Assert.assertEquals(message, messageTextarea.getText());
+    }
+}
